71dd07a76cd9d0a40eeb1f87fcc8269784bc7569
Pradeep Pradyumna <pradeepradyumna@gmail.com>
2020-12-08 16:32:40 +0530

    Adding the basic console application that says my name. LOL

---
+++ SayMyName/SayMyName/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SayMyName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Environment.UserName.ToUpper());
+            Console.ReadLine();
+        }
+    }
+}
